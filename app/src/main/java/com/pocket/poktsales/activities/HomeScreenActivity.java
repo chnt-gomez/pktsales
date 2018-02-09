e3d29a83652cdf50084ef3e9631a997c9f17f63c
Vicente <vicente.martinezgz@gmail.com>
2018-02-08 19:02:13 -0600

    -Starting facebook Interaction

--- app/src/main/java/com/pocket/poktsales/activities/HomeScreenActivity.java
+++ app/src/main/java/com/pocket/poktsales/activities/HomeScreenActivity.java
@@ -1,12 +1,17 @@
 package com.pocket.poktsales.activities;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.CardView;
+import android.util.Base64;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
@@ -31,6 +36,9 @@ import com.pocket.poktsales.utils.Conversor;
 import com.pocket.poktsales.utils.MonthPerformanceMarker;
 
 import org.joda.time.DateTime;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 import butterknife.BindView;
